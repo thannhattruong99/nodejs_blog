281697eae5f815dee116f05808e7776789b86bca
NhatTruong <truongtnse130426@fpt.edu.vn>
2021-11-11 09:39:04 +0700

    Test code fomat auto

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,14 +1,13 @@
-class NewsController{
-
+class NewsController {
     //[GET] /news
-    index(req, res){
+    index(req, res) {
         res.render('news');
     }
 
     //[GET] /news/:slash
-    show(req, res){
+    show(req, res) {
         res.send('NEWS DETAIL!!!');
     }
 }
 
-module.exports = new NewsController;
+module.exports = new NewsController();
